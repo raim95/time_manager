6ab9ae9e4aec2fc20a0acb5a585e5e9d6f6ede48
raim95 <raim9595@gmail.com>
2019-07-09 20:27:49 +0300

    no one listen(

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 url = 'https://matrix.restautomat.ru:18448'
 login = "APoluyanov"
-password = "HIbraltar_1995"
+password = "Hibraltar_1995"
+room_id = "!JtYomtSYacVfwKHWig:matrix.restautomat.ru"
+test_id = "!AyVcYXjBLCeejVbfHb:matrix.restautomat.ru"
--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 from matrix_client.client import MatrixClient
 from matrix_client.room import Room
+#from matrix_client.api import MatrixHttpApi
 import config
 
 work_client = MatrixClient(config.url)
+work_client.login(config.login, config.password)
+#rooms_ids = work_client.rooms
+#work_api=MatrixHttpApi(config.url, work_client)
+
+#rooms_ids_list = []
+
+#for key in rooms_ids.keys():
+#    room_id = str(key).split(":")[0]
+#   rooms_ids_list.append(str(room_id))
+#    room_name = str(work_client.rooms[key].name)
+#    if room_name != "None":
+#       print(str(rooms_ids_list.index(room_id)) + ' - ' + room_id + ' : ' + room_name)
+
+#room_number = int(input('Please, choise room: '))
+#current_room = Room(work_client, rooms_ids_list[room_number])
+#print(current_room.display_name)
+
+room_to_listen = Room( work_client, config.test_id)
+
+def on_message(room, event):
+    if event['type'] == 'm.room.message':
+        print ("Hello!")
+        print(room)
+        print(event)
+
+def main():
+    room_to_listen.add_listener(on_message)
+    work_client.start_listener_thread()
+    work_client.listen_forever()
+
+if __name__ == '__main__':
+    main()
+
+
